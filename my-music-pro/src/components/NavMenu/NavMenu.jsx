46cb747f0a4ac78ff537111e9b07b31f907dc7b4
christinnenko <christinnenko@yandex.ru>
2023-11-21 21:27:00 +0300

    add: emulation loading 2s

--- my-music-pro/src/components/NavMenu/NavMenu.jsx
+++ my-music-pro/src/components/NavMenu/NavMenu.jsx
@@ -1,6 +1,41 @@
 import { useState } from "react";
 import "./NavMenu.css";
 
+//ОСТАВЛЮ ПОКА ЗДЕСЬ, НЕ УВЕРЕНА ЧТО ДЕЛАЮ ПРАВИЛЬНО
+// function NavMenu() {
+//   return (
+//     <nav className="main__nav nav">
+//       <div className="nav__logo logo">
+//         <img className="logo__image" src="/img/logo.png" alt="logo" />
+//       </div>
+//       <div className="nav__burger burger">
+//         <span className="burger__line"></span>
+//         <span className="burger__line"></span>
+//         <span className="burger__line"></span>
+//       </div>
+//       <div className="nav__menu menu">
+//         <ul className="menu__list">
+//           <li className="menu__item">
+//             <a href="#" className="menu__link">
+//               Главное
+//             </a>
+//           </li>
+//           <li className="menu__item">
+//             <a href="#" className="menu__link">
+//               Мой плейлист
+//             </a>
+//           </li>
+//           <li className="menu__item">
+//             <a href="../signin.html" className="menu__link">
+//               Войти
+//             </a>
+//           </li>
+//         </ul>
+//       </div>
+//     </nav>
+//   );
+// }
+
 const NavMenu = () => {
   const [visible, setVisible] = useState(false);
 
--- my-music-pro/src/index.js
+++ my-music-pro/src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./App.css";
-import App from "./App.jsx";
+// import App from "./App.jsx";
+import { ShowEmulationApp } from "./components/EmulationApp/EmulationApp.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ShowEmulationApp />;{/* <App /> */}
   </React.StrictMode>
 );

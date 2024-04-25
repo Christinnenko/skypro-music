cc256ffc0dd168c519d760969319944a5233d24a
christinnenko <christinnenko@yandex.ru>
2024-04-25 16:56:40 +0300

    fix: changes BrowserRouter for deploy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { IndexStyles } from "./index.styles.js";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
@@ -12,9 +12,9 @@ root.render(
     <IndexStyles />
     <React.StrictMode>
       <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </Provider>
     </React.StrictMode>
   </>
